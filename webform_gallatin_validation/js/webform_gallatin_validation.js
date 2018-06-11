12464d0cd958c2ddbb46b53fdacf1c9105864612
Lisa Mackie <lisa.mackie@nyu.edu>
2018-06-11 15:24:51 -0400

    removing logging

--- webform_gallatin_validation/js/webform_gallatin_validation.js
+++ webform_gallatin_validation/js/webform_gallatin_validation.js
@@ -11,13 +11,11 @@
         //Add your custom method with the 'addMethod' function of jQuery.validator
         //http://docs.jquery.com/Plugins/Validation/Validator/addMethod#namemethodmessage
         jQuery.validator.addMethod("validate_netid", function(value, element, param) {
-          console.log("add netid to clientsideValidationAddCustomRules");
           return (/^[A-Za-z]{2,}(\d+)/.test()(value)); 
         }, jQuery.format('Value must be a valid NetID'));
 
 
         jQuery.validator.addMethod("validate_universityid", function(value, element, param) {
-          console.log("add universityID to clientsideValidationAddCustomRules");
           return (/^N\d{8}$/.test()(value)); 
         }, jQuery.format('Value must be a valid University ID'));
 
